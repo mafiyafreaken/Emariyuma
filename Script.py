12004939a9a6da88049f28adc2fb4331b9633d41
mafiyafreaken <46566331+mafiyafreaken@users.noreply.github.com>
2022-08-04 00:18:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,8 +13,6 @@ class script(object):
 - ok
 - bye  
 
-<b>DEVS:</b>
-- <a href=https://t.me/FILMCOMPANYCHANNEL> Updates </a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
@@ -39,7 +37,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/mc_filter1bot)</code>
+<code>[Button Text](buttonurl:https://t.me/killadi_soman)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
